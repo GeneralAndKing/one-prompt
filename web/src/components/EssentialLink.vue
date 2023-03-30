a8448b68e8fe3a76176bc299ecbda349313e29d3
yue <lizhongyue248@163.com>
2023-03-30 10:30:04 +0800

    feat(project): Begin a project.

---
+++ web/src/components/EssentialLink.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+q-item(clickable='' tag='a' target='_blank' :href='link')
+  q-item-section(v-if='icon' avatar='')
+    q-icon(:name='icon')
+  q-item-section
+    q-item-label {{ title }}
+    q-item-label(caption='') {{ caption }}
+</template>
+
+<script setup lang="ts">
+export interface EssentialLinkProps {
+  title: string;
+  caption?: string;
+  link?: string;
+  icon?: string;
+}
+withDefaults(defineProps<EssentialLinkProps>(), {
+  caption: '',
+  link: '#',
+  icon: ''
+})
+</script>
---
+++ web/src/components/ExampleComponent.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  div
+    p {{ title }}
+    ul
+      li(v-for='todo in todos' :key='todo.id' @click='increment')
+        | {{ todo.id }} - {{ todo.content }}
+    p Count: {{ todoCount }} / {{ meta.totalCount }}
+    p Active: {{ active ? 'yes' : 'no' }}
+    p Clicks on todos: {{ clickCount }}
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import { Todo, Meta } from './models'
+
+interface Props {
+  title: string;
+  todos?: Todo[];
+  meta: Meta;
+  active: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  todos: () => []
+})
+
+const clickCount = ref(0)
+function increment () {
+  clickCount.value += 1
+  return clickCount.value
+}
+
+const todoCount = computed(() => props.todos.length)
+
+</script>
