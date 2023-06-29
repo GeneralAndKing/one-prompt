46e6e29d7abaf68fc4fb64e3bb865a9b246481df
中月不吃辣 <lizhongyue248@163.com>
2023-06-30 00:39:25 +0800

    feat(mock): Add page mock.

--- web/src/pages/ContentPage/ContentPage.vue
+++ web/src/pages/ContentPage/ContentPage.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import { onMounted, reactive, ref } from 'vue'
+import { computed, onMounted, reactive, ref, watch, watchEffect } from 'vue'
 import { useI18n } from 'vue-i18n'
-import { ChannelSearchParam, search, SearchResponse } from '@/api/Channel'
+import {
+  ChannelFilter,
+  ChannelFilterType,
+  ChannelSearchParam,
+  ChannelTab,
+  getAllChannel,
+  search,
+  SearchResponse
+} from '@/api/Channel'
 import useBoolean from '@/hooks/useBoolean'
 
 const { t } = useI18n({ useScope: 'global' })
 const route = useRoute()
 const form = reactive<ChannelSearchParam>({
-  engine: 'chatGPT',
+  engine: '',
   keywords: '',
   trending: [],
   pageNow: 1,
@@ -16,17 +24,82 @@ const form = reactive<ChannelSearchParam>({
   filters: {}
 })
 const view = ref('one')
-
-onMounted(() => {
-  const engine = route.params.engine
-  console.log(engine)
-  handleSearch()
+const data = ref<SearchResponse>({
+  list: [],
+  pageNow: 1,
+  pageSize: 10,
+  total: 0,
+  totalPage: 0
 })
-
-const data = ref<SearchResponse[]>([])
+const channelTabs = ref<ChannelTab[]>([])
+const { state: showFilters, toggle: toggleFilters } = useBoolean(false)
 const { state: loading, setTrue: start, setFalse: finish } = useBoolean()
 
-const handleSearch = async () => {
+const resetFilters = () => {
+  if (form.filters === null) {
+    form.filters = {}
+  }
+  for (const channel of channelTabs.value) {
+    for (const filter of channel.filters.filter(item => item.type === ChannelFilterType.CHECKBOX)) {
+      if (!filter.options) {
+        continue
+      }
+      for (const option of filter.options) {
+        form.filters[option.name] = false
+      }
+    }
+    for (const filter of channel.filters.filter(item => item.type === ChannelFilterType.TOGGLE)) {
+      form.filters[filter.name] = false
+    }
+  }
+}
+
+onMounted(async () => {
+  channelTabs.value = await getAllChannel()
+  const engine = route.params.engine
+  if (engine === '') {
+    form.engine = channelTabs.value[0].name
+  } else {
+    const find = channelTabs.value.map(item => item.name).find(item => item === engine)
+    if ((find && find.length === 0) || !find) {
+      form.engine = channelTabs.value[0].name
+    } else {
+      form.engine = engine[0]
+    }
+  }
+  await resetFilters()
+  await handleSearch()
+})
+
+const currentTab = computed<ChannelTab | null>(() => {
+  if (!channelTabs.value || !form.engine) {
+    return null
+  }
+  return channelTabs.value.find(item => item.name === form.engine) || null
+})
+
+interface ChannelTabFilters {
+  select: ChannelFilter[],
+  checkbox: ChannelFilter[],
+  toggle: ChannelFilter[]
+}
+
+const currentFilters = computed<ChannelTabFilters>(() => {
+  if (!currentTab.value) {
+    return {
+      select: [],
+      checkbox: [],
+      toggle: []
+    }
+  }
+  return {
+    select: currentTab.value?.filters.filter(item => item.type === ChannelFilterType.SELECT) || [],
+    checkbox: currentTab.value?.filters.filter(item => item.type === ChannelFilterType.CHECKBOX) || [],
+    toggle: currentTab.value?.filters.filter(item => item.type === ChannelFilterType.TOGGLE) || []
+  }
+})
+const handleSearch = async (pageName = 1) => {
+  form.pageNow = pageName
   start()
   try {
     data.value = await search(form)
@@ -34,6 +107,14 @@ const handleSearch = async () => {
     finish()
   }
 }
+watch(() => form.engine, async () => {
+  form.keywords = ''
+  form.trending = []
+  form.pageNow = 1
+  form.pageSize = 1
+  await resetFilters()
+  await handleSearch()
+})
 </script>
 
 <template>
@@ -46,17 +127,15 @@ const handleSearch = async () => {
       narrow-indicator
       align="left"
     >
-      <q-tab name="chatGPT" label="chatGPT" />
-      <q-tab name="Mid" label="Mid" />
-      <q-tab name="文心一言" label="文心一言" />
+      <q-tab v-for="tab in channelTabs" :key="tab.id" :name="tab.name" :label="tab.name"/>
     </q-tabs>
-    <q-separator />
+    <q-separator/>
     <div class="form q-mt-md row q-gutter-x-lg">
-      <q-btn outline class="col-2">
-        <q-icon name="filter_list" />
+      <q-btn outline class="col-2" @click="toggleFilters">
+        <q-icon name="filter_list"/>
         Show Filters
       </q-btn>
-      <q-input class="col-7"
+      <q-input class="col"
                v-model="form.keywords"
                :placeholder="t('page.chat.search')"
                outlined
@@ -71,7 +150,7 @@ const handleSearch = async () => {
         </template>
       </q-input>
 
-      <q-btn-dropdown outline color="col-1" label="Trending">
+      <q-btn-dropdown outline class="col-1" label="Trending">
         <q-list>
           <q-item clickable v-close-popup>
             <q-item-section>
@@ -102,122 +181,118 @@ const handleSearch = async () => {
         ]"
       >
         <template v-slot:one>
-          <q-icon name="window" />
+          <q-icon name="window"/>
         </template>
         <template v-slot:two>
-          <q-icon name="calendar_view_week" />
+          <q-icon name="calendar_view_week"/>
         </template>
       </q-btn-toggle>
     </div>
     <div class="row flex flex-nowrap">
-      <div class="filter col-3">
+      <div :class="showFilters ? '': 'hidden'" class="filter col-2">
         <q-list padding>
-          <q-item>
-            <q-item-section>
-              <q-item-label>ChatGPT Version</q-item-label>
-            </q-item-section>
-            <q-item-section side>
-              <q-select
-                model-value="3.5"
-                :options="['3.5', '4.0']"
-                hide-dropdown-icon
-                dense
-                style="width: 40px"
-              >
-                <template v-slot:selected>
-                  <div class="full-width text-center">
-                    3.5
-                  </div>
-                </template>
-              </q-select>
-            </q-item-section>
-          </q-item>
-          <q-separator spaced />
-          <q-item-label header>Category</q-item-label>
-          <q-item tag="label" dense v-ripple>
-            <q-item-section side top>
-              <q-checkbox />
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Computer Science</q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item tag="label" dense v-ripple>
-            <q-item-section side top>
-              <q-checkbox />
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Humanities</q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item tag="label" dense v-ripple>
-            <q-item-section side top>
-              <q-checkbox />
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Role Assignment</q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-separator spaced />
-          <q-item-label header>Use Method</q-item-label>
-          <q-item tag="label" dense v-ripple>
-            <q-item-section side top>
-              <q-checkbox />
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>One Prompt</q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item tag="label" dense v-ripple>
-            <q-item-section side top>
-              <q-checkbox />
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Model Training</q-item-label>
-            </q-item-section>
-          </q-item>
-          <q-item tag="label" dense v-ripple>
-            <q-item-section side top>
-              <q-checkbox />
-            </q-item-section>
-
-            <q-item-section>
-              <q-item-label>Notifications</q-item-label>
-            </q-item-section>
-          </q-item>
+          <template v-for="item in currentFilters.toggle" :key="`toggle-${item.id}`">
+            <q-item>
+              <q-item-section>
+                <q-item-label>{{ item.name }}</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-toggle
+                  dense
+                  :true-value="true"
+                  :false-value="false"
+                  v-model="form.filters[item.name]"
+                />
+              </q-item-section>
+            </q-item>
+          </template>
+          <template v-for="(item, index) in currentFilters.select" :key="`select-${item.id}`">
+            <q-separator v-if="index === 0 && currentFilters.toggle.length > 0" spaced/>
+            <q-item>
+              <q-item-section>
+                <q-item-label>{{ item.name }}</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-select
+                  v-model="form.filters[item.name]"
+                  :options="item.options"
+                  hide-dropdown-icon
+                  option-label="name"
+                  option-value="value"
+                  dense
+                  :style="form.filters[item.name] || { width: '40px' }"
+                >
+                  <template v-slot:selected>
+                    <div class="full-width text-center">
+                      {{ form.filters[item.name]?.name }}
+                    </div>
+                  </template>
+                </q-select>
+              </q-item-section>
+            </q-item>
+          </template>
+          <template v-for="(item, index) in currentFilters.checkbox" :key="`checkbox-${item.id}`">
+            <q-separator v-if="index === 0 && currentFilters.select.length > 0" spaced/>
+            <q-expansion-item
+              :model-value="index === 0"
+              :label="item.name"
+            >
+              <div class="q-mt-sm">
+                <q-item v-for="option in item.options" :key="option.id" tag="label" dense v-ripple>
+                  <q-item-section side top>
+                    <q-checkbox :true-value="true" :false-value="false" dense v-model="form.filters[option.name]"/>
+                  </q-item-section>
+                  <q-item-section>
+                    <q-item-label>{{ option.name }}</q-item-label>
+                  </q-item-section>
+                </q-item>
+              </div>
+            </q-expansion-item>
+            <q-separator v-if="index !== currentFilters.checkbox.length - 1" spaced/>
+          </template>
         </q-list>
       </div>
-      <div class="data-list q-mt-lg col">
-        <q-card v-for="item in data" :key="item.id" class="col-6">
-          <q-card-section>
-            <div class="full-width flex justify-center align-middle">
-              <q-chip size="sm" label="Hello" outline />
-              <q-space />
-              <q-icon size="sm" name="list" />
-            </div>
-            <div class="q-pt-xs row q-gutter-x-lg">
-              <div class="col flex justify-between">
-                <div class="text-h6">{{ item.name }}</div>
-                <div class="ellipsis-3-lines" style="height: calc(1em * 3 * 1.5)">
-                  {{ item.description }}
+      <div class="q-mt-lg q-ml-lg col">
+        <div v-show="!loading" class="data-list">
+          <q-card v-for="item in data.list" :key="item.id" class="col-6">
+            <q-card-section class="flex column full-height align-baseline content-start">
+              <div class="full-width flex justify-center align-middle" style="flex: 0">
+                <q-chip size="sm" label="Hello" outline/>
+                <q-space/>
+                <q-icon size="sm" name="list"/>
+              </div>
+              <div class="q-pt-xs row q-gutter-x-lg" style="flex: 1">
+                <div class="col flex flex-row justify-between">
+                  <div class="text-h6 ellipsis-2-lines full-width"
+                       style="height: calc(1em * 2 * 1.5);line-height: 1.5em">{{ item.name }}
+                  </div>
+                  <div class="flex-1">
+                    <div class="ellipsis-3-lines" style="height: calc(1em * 3 * 1.5);flex: 1">
+                      {{ item.description }}
+                    </div>
+                  </div>
+                  <div class="q-mt-sm">
+                    <q-avatar size="sm" class="q-mr-xs">
+                      <q-img v-if="item.avatar" :src="item.avatar"/>
+                    </q-avatar>
+                    <span class="text-grey-7">{{ item.author }}</span>
+                  </div>
                 </div>
-                <div>
-                  <q-avatar size="sm" class="q-mr-xs">
-                    <q-img v-if="item.avatar" :src="item.avatar" />
-                  </q-avatar>
-                  <span class="text-grey-7">
-                    {{ item.author }}
-                  </span>
+                <div v-if="item.image" class="col-3 flex items-center justify-center">
+                  <q-img :alt="item.name" :src="item.image"/>
                 </div>
               </div>
-              <div class="col-3">
-                <q-img v-if="item.image" :src="item.image" />
-              </div>
-            </div>
-          </q-card-section>
-        </q-card>
+            </q-card-section>
+          </q-card>
+        </div>
+        <div class="full-width flex q-mt-lg text-center justify-center items-center">
+          <q-pagination
+            v-model="form.pageNow"
+            :max="data.totalPage"
+            input
+            @update:model-value="handleSearch"
+          />
+        </div>
       </div>
     </div>
   </q-page>
@@ -226,7 +301,8 @@ const handleSearch = async () => {
 <style scoped lang="scss">
 .data-list {
   display: grid;
-  grid-template-columns: repeat(2, calc( 50% - 14px));
+  grid-template-columns: repeat(3, calc(33.33% - 14px));
+  grid-auto-rows: auto;
   grid-gap: 24px;
 }
 </style>
